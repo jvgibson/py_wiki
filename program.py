a2dbc873014ecfa8ad6c9ddca2ebc61d4fe0a819
Joey <madavik@gmail.com>
2013-05-04 16:43:45 -0600

    comments to program.py

--- program.py
+++ program.py
@@ -2,17 +2,17 @@
 import sys
 
 
-def beef(a):
-  print "beef"
-  print a
-
-
-
-
 
 def main(argc=None):
-  beef(4)
+#get the users input
+ 
+#check the users input
 
+#ckeck wiki api for search words in a city
+
+#check the pages for the top 20 most used words
+
+#print out the words and count
 
 if __name__ == "__main__":
   sys.exit(main())
