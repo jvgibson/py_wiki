82ee1b83ba70896e0feca52566810781c7c3b3c3
Joey <madavik@gmail.com>
2013-05-04 19:27:30 -0600

    input checking

--- program.py
+++ program.py
@@ -2,17 +2,35 @@
 import sys
 
 
+def checkInput(city, state):
 
+  if city.lower() in open('usCities.txt').read().lower() and state.lower() in open('usStates.txt').read().lower():
+    return True
+  else:
+    return False
+
+#Main function
 def main(argc=None):
-#get the users input
- 
-#check the users input
+  #get the users input
+  
+  check = False
 
-#ckeck wiki api for search words in a city
+  while(not check):
+    try:
 
-#check the pages for the top 20 most used words
+      userLocation = raw_input('Enter a US City and State (such as "Seattle, WA"): ')
+      uCity, uState = userLocation.split(",")
+      check = checkInput(uCity.strip(),uState.strip())
+      print userLocation
+    except:
+      print 'Invalid Input, Please try again'
+  #check the users input
 
-#print out the words and count
+  #ckeck wiki api for search words in a city
+
+  #check the pages for the top 20 most used words
+
+  #print out the words and count
 
 if __name__ == "__main__":
   sys.exit(main())
