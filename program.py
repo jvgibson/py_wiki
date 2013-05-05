f92d9b75be40261319aab97bef3ce9e496f363bf
Joey <madavik@gmail.com>
2013-05-05 15:12:46 -0600

    added top web search

--- program.py
+++ program.py
@@ -1,5 +1,21 @@
 #! /usr/bin/env python
 import sys
+import urllib
+import json
+
+
+def showsome(searchfor):
+    query = urllib.urlencode({'q': searchfor})
+    url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&%s' % query
+    search_response = urllib.urlopen(url)
+    search_results = search_response.read()
+    results = json.loads(search_results)
+    data = results['responseData']
+    print 'Total results: %s' % data['cursor']['estimatedResultCount']
+    hits = data['results']
+    print 'Top %d hits:' % len(hits)
+    for h in hits: print ' ', h['url']
+    print 'For more results, see %s' % data['cursor']['moreResultsUrl']
 
 
 def checkInput(city, state):
@@ -11,22 +27,27 @@ def checkInput(city, state):
 
 #Main function
 def main(argc=None):
-  #get the users input
   
   check = False
 
   while(not check):
     try:
-
+           
+      #get the users input
       userLocation = raw_input('Enter a US City and State (such as "Seattle, WA"): ')
+
+      #seperates the city and state
       uCity, uState = userLocation.split(",")
+      
+      #check the users input
       check = checkInput(uCity.strip(),uState.strip())
-      print userLocation
+      
+      #grabs the top sites on wikipedia
+      userLocation = userLocation + ' site:wikipedia.org'
+      showsome(userLocation)
     except:
       print 'Invalid Input, Please try again'
-  #check the users input
 
-  #ckeck wiki api for search words in a city
 
   #check the pages for the top 20 most used words
 
